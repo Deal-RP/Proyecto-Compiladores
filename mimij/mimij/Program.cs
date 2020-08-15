20bd6b39627500fb714d350e45a633b7ecd1083d
MacheteZ5 <machetemontenegro@hotmail.com>
2020-08-14 19:00:12 -0600

    Omitir espacios en blanco

--- mimij/mimij/Program.cs
+++ mimij/mimij/Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace mimij
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Escriba el código");
+            //ignorar espacios en blanco, saltos de línea, y tabs
+            var frase = Console.ReadLine();
+            var delimitadores = new char[]{ ' ', '\t', '\n' };
+            var Split = frase.Split(delimitadores);
+            var resultL = new List<string>();
+            for(int i = 0; i < Split.Length; i++)
+            {
+                if (Split[i] != string.Empty)
+                {
+                    resultL.Add(Split[i]);
+                }
+            }
+            //empezar el proceso de validación por lexema
+            var process = new Validation();
+            foreach(string lexema in resultL)
+            {
+                process.validar(lexema);
+            }
         }
     }
 }
---
+++ mimij/mimij/Validation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+namespace mimij
+{
+    public class Validation
+    {
+        public Validation()
+        {
+
+        }
+
+        public bool validar(string lexema)
+        {
+            var valido = false;
+            string pattern;
+            //expresión regular con Regex
+            for (int i = 0; i < 5; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        //para Palabras reservadas
+                        pattern = "^void$|^int$|^double$|^boolean$|^string$|^class$|^const$|^interface$|^null$|^this$|^extends$|^implements$|^for$|^while$|^if$|^else$|^return$|^break$|^New$|^System$|^out$|^println$";
+                        valido = Regex.IsMatch(lexema, pattern);
+                        Console.WriteLine(valido);
+                        Console.ReadLine();
+                        //mandar a escribir al archivo de aquí de una vez o vos que pensas, dime??
+                        break;
+                    case 1:
+                        //constantes booleanas
+                        pattern = "^true$|^false$";
+                        valido = Regex.IsMatch(lexema, pattern);
+                        Console.WriteLine(valido);
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        //para numeros
+                        pattern = "^[0-9]+$|^0[X|x][0-9A-F]+$|^[0-9]+.[0-9]+$|^[0-9]+.([0-9]+)?[E|e]\\+[0-9]{1,2}$";
+                        valido = Regex.IsMatch(lexema, pattern);
+                        Console.WriteLine(valido);
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        pattern = "^\\+$|^\\-$|^\\/$|^\\*$|^\\%$|^\\<$|^\\<=$|^\\>$|^\\>\\=$|^\\=$|^\\=\\=$|^\\!=$|^\\&\\&$|^\\|\\|$|^\\!$|^\\;$|^\\,$|^\\.$|^\\[\\]$|^\\[$|^\\]|^\\(\\)$|^\\{$|^\\}$|^\\{\\}$|^\\($|^\\)$";
+                        valido = Regex.IsMatch(lexema, pattern);
+                        Console.WriteLine(valido);
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        // para nombre de variables (Identificadores)
+                        pattern = "^([A-z]){1,31}$|^[A-z]([A-z0-9]){2,31}$|^\\$[A-z0-9]{2,31}$";
+                        valido = Regex.IsMatch(lexema, pattern);
+                        Console.WriteLine(valido);
+                        Console.ReadLine();
+                        break;
+                }
+                if (valido)
+                {
+                    break;
+                }
+            }
+            return valido;
+        }
+    }
+}
