d6592c061e2db4f51e8542d3b28ac5ae8b8fca80
Deal-RP <derly.rodas@gmail.com>
2020-08-18 22:24:27 -0600

    Validaciones v1.0

--- mimij/mimij/Program.cs
+++ mimij/mimij/Program.cs
@@ -30,106 +30,74 @@ namespace mimij
                         var start = 0;
                         var i = 1;
                         var lexema = string.Empty;
+
                         while (start != frase.Length && i < frase.Length + 1)
                         {
                             lexema = frase.Substring(start, i - start);
-                            var tipo = Validation.validar(lexema);
+                            var tipo = Validation.identificar(lexema);
+
                             if (tipo == -1)
                             {
-                                if (frase[i - 1] == '\t' || frase[i - 1] == ' ')
+                                var auxTipo = Validation.identificar(frase.Substring(start, i - start - 1));
+
+                                switch (auxTipo)
                                 {
-                                    if (lexema.Length > 1)
-                                    {
+                                    case -1:
+                                        //CARACTER INVALIDO
+                                        break;
+                                    case 2:
+                                        if (frase[i - 1] == '\t' || frase[i - 1] == ' ' || Validation.identificar(frase[i - 1].ToString()) == 7)
+                                        {
+                                            lexema = frase.Substring(start, i - start - 1);
+                                            if (frase[i - 1] != '\t' && frase[i - 1] != ' ') i--;
+                                        }
+                                        else
+                                        {
+                                            //ERROR
+                                        }
+                                        break;
+                                    case 4:
+                                        //NUMERO HEXADECIMAL INCOMPLETO
+                                        break;
+                                    case 6:
+                                        //DOUBLE INCOMPLETO
+                                        break;
+                                    case 10:
+                                        //STRING INCOMPLETO
+                                        break;
+                                    default:
                                         lexema = frase.Substring(start, i - start - 1);
-                                    }
-                                    lexema = string.Empty;
-                                    start = i;
-                                }
-                                else
-                                {
-                                    if (lexema.Length > 0)
-                                    {
-                                        //if hexadecimales
-                                        if (lexema.Last() == 'x' || lexema.Last() == 'X')
-                                        {
-                                            while (tipo != -1 && i < frase.Length)
-                                            {
-                                                lexema += frase[i];
-                                                tipo = Validation.validar((lexema));
-                                                if (tipo != -1) i++;
-                                            }
-                                        }
-                                        //double con exponente
-                                        else if (lexema.Last() == 'e' || lexema.Last() == 'E')
-                                        {
-                                            var ER = "^[0-9]+\\.[0-9]*[E|e]$";
-                                            if (Regex.IsMatch(lexema, ER))
-                                            {
-                                                while (tipo != -1 && i < frase.Length)
-                                                {
-                                                    lexema += frase[i];
-                                                    tipo = Validation.validar(lexema);
-                                                    lexema = (tipo != -1) ? lexema : lexema.Substring(0, lexema.Length - 1);
-                                                    i = (tipo != -1) ? i + 1 : i;
-                                                }
-                                            }
-                                        }
-                                        // tercero para strings
-                                        else if (lexema.Last() == '"')
-                                        {
-                                            var LAux = string.Empty;
-                                            LAux += lexema.Last();
-                                            lexema = lexema.Substring(0, lexema.Length - 1);
-                                            //mandar a escribir el lexema.
-                                            do
-                                            {
-                                                LAux += frase[i];
-                                                i++;
-                                            } while (LAux.Last() != '"' && i < frase.Length);
-                                            tipo = Validation.validar(LAux);
-                                            if (tipo==-1)
-                                            {
-                                                //string sin cerrar
-                                            }
-                                            else
-                                            {
-                                                //mandar a escribir el resto
-                                            }
-                                        }
-                                        //cuarto para comentarios //
-                                        else if (lexema.Last() == '/' && frase[i] == '/')
-                                        {
-                                            //Almacenar el último valor
-                                            var LAux = string.Empty;
-                                            LAux += lexema.Last();
-                                            lexema = lexema.Substring(0, lexema.Length - 1);
-                                            //mandar a escribir el lexema.
-                                            while (i < frase.Length)
-                                            {
-                                                LAux += frase[i];
-                                                i++;
-                                            }
-                                            //mandar a escribir LAux
-                                        }
-                                        //quinto para comentario con *
-                                        else if((lexema.Last() == '/'&&frase[i]=='*')/*||(lexema.Last()=='*'&&lexema[lexema.Length-2]=='/')*/)
-                                        {
-                                            var LAux = string.Empty;
-                                            LAux += lexema.Last();
-                                            lexema = lexema.Substring(0, lexema.Length - 1);
-                                            //mandar a escribir el lexema.
-                                        }
-                                        //Validation.fileWriting(txtName, lexema, countLinea, start, i, tipo);
-                                        start = i;
+                                        if (frase[i - 1] != '\t' && frase[i - 1] != ' ') i--;
                                         lexema = string.Empty;
-                                    }
+                                        break;
                                 }
+                                start = i;
+                                //Validation.fileWriting(txtName, lexema, countLinea, start, i, auxTipo);
                             }
                             i++;
                         }
+
                         if (lexema.Length != 0)
                         {
-                            var tipo = Validation.validar(lexema);
+                            var tipo = Validation.identificar(lexema);
+                            switch (tipo)
+                            {
+                                case -1:
+                                    //CARACTER INVALIDO
+                                    break;
+                                case 4:
+                                    //NUMERO HEXADECIMAL INCOMPLETO
+                                    break;
+                                case 6:
+                                    //DOUBLE INCOMPLETO
+                                    break;
+                                case 10:
+                                    //STRING INCOMPLETO
+                                    break;
+                                default:
+                                    lexema = frase.Substring(start, i - start - 1);
+                                    break;
+                            }
                             //Validation.fileWriting(txtName, lexema, countLinea, frase.Length - lexema.Length, frase.Length, tipo);
                             lexema = string.Empty;
                         }
--- mimij/mimij/Validation.cs
+++ mimij/mimij/Validation.cs
@@ -13,7 +13,7 @@ namespace mimij
     {
         //Nombre, linea, columnaInicial, columnaFinal, atributo
         //static Tuple<string, int, int, int, string> tablaOut;
-        static public int validar(string lexema)
+        static public int identificar(string lexema)
         {
             var caracteres = "\0\u0001\u0002\u0003\u0004\u0005\u0006\a\b\t\n\v\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f !#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{\\|}~\u007f\u0080\u0081\u0082\u0083\u0084\u0085\u0086\u0087\u0088\u0089\u008a\u008b\u008c\u008d\u008e\u008f\u0090\u0091\u0092\u0093\u0094\u0095\u0096\u0097\u0098\u0099\u009a\u009b\u009c\u009d\u009e\u009f ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
             var listER = new List<string>()
@@ -26,19 +26,24 @@ namespace mimij
                 "^[0-9]+$",
                 //HEXADECIMAL
                 "^0[X|x][0-9A-Fa-f]+$",
+                //POSIBLE HEXADECIMAL
+                "^0[X|x]$",
                 //DOUBLE
-                "^[0-9]+\\.[0-9]*$|^[0-9]+\\.[0-9]*[E|e][+|-]?[0-9]{1,2}$",
+                "^[0-9]+\\.[0-9]*([E|e][+|-]?[0-9]{1,2})?$",
+                //POSIBLE DOUBLE
+                "^[0-9]+\\.[0-9]*[E|e][+|-]?",
                 //OPERADORES
                 "^\\+$|^\\-$|^\\/$|^\\*$|^\\%$|^\\<$|^\\<=$|^\\>$|^\\>\\=$|^\\=$|^\\=\\=$|^\\!=$|^\\&\\&$|^\\|\\|$|^\\!$|^\\;$|^\\,$|^\\.$|^\\[\\]$|^\\[$|^\\]|^\\(\\)$|^\\{$|^\\}$|^\\{\\}$|^\\($|^\\)$",
-                //Prueba
                 //IDENTIFICADOR
                 "^([A-z]){1,31}$|^[A-z]([A-z0-9]){2,31}$|^\\$[A-z0-9]{2,31}$",
                 //STRING
-                $"^\"[{caracteres}]+\"$",
+                $"^\"[{caracteres}]*\"$",
+                //POSIBLE STRING
+                $"^\"[{caracteres}]*$",
                 //comentario //
                 $"^\\/\\/[{caracteres}]*$",
                 //comentario/*
-                $"^\\/\\*[{caracteres}]\\*\\/$"
+                $"^\\/\\*[{caracteres}]\\*\\/$",
             };
 
             var cont = 0;
